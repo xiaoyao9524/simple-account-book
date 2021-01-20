aa664e213d72f42ddddf9b752148f10e26c406fc
xiaoyao <xiaoyao9524@163.com>
2021-01-20 10:32:07 +0800

    修改合并前后端项目

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { DndProvider } from 'react-dnd';
-import { TouchBackend } from 'react-dnd-touch-backend';
 import store from './store';
 import 'normalize.css/normalize.css'
 import './styles/index.scss';
 import App from './App';
 
 ReactDOM.render(
-  <DndProvider backend={TouchBackend} >
     <Provider store={store}>
       <App />
-    </Provider>
-  </DndProvider>,
+    </Provider>,
   document.getElementById('root')
 );
 
