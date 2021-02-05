2eb49865d7891e5eb19aa15a22e228c5073d167b
xiaoyao <xiaoyao9524@163.com>
2021-02-05 23:30:33 +0800

    修改config

--- server/config/config.default.ts
+++ server/config/config.default.ts
@@ -27,28 +27,7 @@ export default (appInfo: EggAppInfo) => {
     domainWhiteList: ['http://localhost:3000'], //允许访问接口的白名单
   };
 
-  config.sequelize = {
-    dialect: 'mysql', // 指定数据源
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '123456',
-    database: 'simple_account_book',
-    define: {
-      timestamps: false, // 不需要自动为我们添加时间相关字段
-      freezeTableName: true, // 使用原来的表名称，不需要sequelize处理表名称
-    },
-    timezone: '+08:00',
-  };
-
-  config.redis = {
-    client: {
-      port: 6379,
-      host: 'localhost',
-      password: '123456',
-      db: 0, // 默认为0
-    },
-  };
+  
 
   config.validatePlus = {
     resolveError(ctx, errors) {
--- server/config/config.local.ts
+++ server/config/config.local.ts
@@ -2,5 +2,29 @@ import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
+
+  config.sequelize = {
+    dialect: 'mysql', // 指定数据源
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '123456',
+    database: 'simple_account_book',
+    define: {
+      timestamps: false, // 不需要自动为我们添加时间相关字段
+      freezeTableName: true, // 使用原来的表名称，不需要sequelize处理表名称
+    },
+    timezone: '+08:00',
+  };
+
+  config.redis = {
+    client: {
+      port: 6379,
+      host: 'localhost',
+      password: '123456',
+      db: 0, // 默认为0
+    },
+  };
+  
   return config;
 };
