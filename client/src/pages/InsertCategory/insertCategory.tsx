91d2eea1955c0ef42d7c7a20bb55978bb086dd94
xiaoyao <xiaoyao9524@163.com>
2021-02-14 00:45:42 +0800

    提交

--- client/src/pages/InsertCategory/insertCategory.tsx
+++ client/src/pages/InsertCategory/insertCategory.tsx
@@ -1,5 +1,12 @@
-import React, { useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory, useLocation } from "react-router-dom";
+
+/** components */
+import {
+  List,
+  InputItem,
+  WhiteSpace
+} from "antd-mobile";
 import NavBar from '../../components/NavBar'
 
 /** components */
@@ -10,17 +17,135 @@ import {
 /** style */
 import './style.scss';
 
+const Item = List.Item;
+const Brief = Item.Brief
+
 interface ILocalState {
   type?: '支出' | '收入';
 }
+
+const expenditure = [
+  {
+    title: '娱乐',
+    list: [
+      'shuma',
+      'game',
+      'steam',
+      'uplay',
+      'wegame',
+      'gog',
+      'epic',
+      'origin'
+    ]
+  },
+  {
+    title: '饮食',
+    list: [
+      'canyin',
+      'shuiguo',
+      'shucai',
+      'lingshi',
+      'cha',
+      'yanjiu'
+    ]
+  },
+  {
+    title: '医疗',
+    list: [
+      'yiliao'
+    ]
+  },
+  {
+    title: '学习',
+    list: [
+      'shuji',
+      'xuexi'
+    ]
+  },
+  {
+    title: '交通',
+    list: [
+      'zixingche',
+      'chuzuche',
+      'gongjiao',
+      'ditie',
+      'huoche',
+      'jipiao',
+      'qiche',
+      'truck'
+    ]
+  },
+  {
+    title: '购物',
+    list: [
+      'fushi',
+      'lvxing',
+      'gouwu',
+      'taobao',
+      'jingdong',
+      'chaoshi',
+      'riyongpin',
+      'liwu'
+    ]
+  },
+  {
+    title: '生活',
+    list: [
+      'shuifei',
+      'dianfei',
+      'wangluo',
+      'huafei',
+      'meirong'
+    ]
+  },
+  {
+    title: '家居',
+    list: [
+      'zhufang',
+      'jujia',
+      'weixiu'
+    ]
+  },
+  {
+    title: '家庭',
+    list: [
+      'haizi',
+      'zhangbei',
+      'chongwu',
+      'qinyou'
+    ]
+  },
+  {
+    title: '健身',
+    list: [
+      'yundong'
+    ]
+  },
+  {
+    title: '办公',
+    list: [
+      'bangong'
+    ]
+  },
+  {
+    title: '其它',
+    list: [
+      'jiaofei',
+      'qita',
+      'shejiao',
+      'lijin',
+      'juanzeng',
+      'zhuanzhang',
+      'caipiao'
+    ]
+  }
+]
 
 const InsertCategory = () => {
   const history = useHistory();
   const location = useLocation<ILocalState>();
 
   const state = location.state;
-  console.log('state: ', state);
-  
 
   useEffect(() => {
     if (!state || !state.type) {
@@ -28,10 +153,37 @@ const InsertCategory = () => {
     }
   })
 
+  const categoryType = useState<0 | 1>(state.type === '收入' ? 0 : 1);
+
   return (
     <div className="insert-category">
-      <NavBar>新增类别</NavBar>
-      新增类别
+      <NavBar style={{ position: 'fixed', top: 0, left: 0, width: '100%', zIndex: 5 }}>新增类别</NavBar>
+      <List style={{ position: 'fixed', top: 45, left: 0, width: '100%', zIndex: 5 }}>
+        <InputItem
+          clear
+          placeholder="请输入类别名称"
+        >类别名称</InputItem>
+      </List>
+      <ul className="icon-list-wrapper">
+        {expenditure.map(i => (
+          <li key={i.title} className="icon-list-item">
+            <h3 className="icon-title">{i.title}</h3>
+            <ul className="icon-list">
+              {
+                i.list.map(icon => (
+                  <li
+                    key={icon}
+                    className="icon-item"
+                    onClick={() => { console.log(icon) }}
+                  >
+                    <span className={`icon iconfont icon-${icon}`}></span>
+                  </li>
+                ))
+              }
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
