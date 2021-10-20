50cfc0313ccef99366f36c4e28324f541a664e79
xiaoyao <xiaoyao9524@163.com>
2021-10-20 23:39:17 +0800

    优化项目结构

--- client/src/components/Calculator/index.old.ts
+++
@@ -1,5 +0,0 @@
-// import Calculator, {a} from './calculator';
-
-// export default Calculator;
-
-export default 'hello world';
--- client/src/components/TabBar/tabBar.tsx
+++ client/src/components/TabBar/tabBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 
-import { IStoreState } from '../../store/reducers/index'
+import { IStoreState } from '@/store/reducers/index'
 import {
   NavLink
 } from 'react-router-dom';
